f5dbe7ab7334fc02fdbc6d7b39e7ebb412a75275
Sandev <sandevdewthilina2000@gmail.com>
2023-10-19 22:28:05 +0530

    changes

--- src/records/recorder.py
+++
@@ -1,9 +0,0 @@
-from src.database.operations import add_record
-
-
-class Recorder:
-
-    @staticmethod
-    def create_record(user, record):
-        record['sensitivity_level'] = user['privilege_level']
-        add_record(record)
